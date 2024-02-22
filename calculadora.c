2732a17b615253d602a4928e23ac7dc467ad68b4
SERAPHINE25 <160934468+SERAPHINE25@users.noreply.github.com>
2024-02-22 12:59:16 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -5,13 +5,23 @@
 int main()
 {
   int opc;
+  int var1=0, var2=0;
   printf("\n\t\t BIENVENIDO \n");
   printf("\n\t 1 - SUMA");
   printf("\n\t 2 - RESTA");
   printf("\n\t 3 - MULTIPLICACIÓN");
   printf("\n\t 4 - DIVISION");
-  printf("\n\t Ingresa la opción a realizar")
+  printf("\n\t Ingresa la opción a realizar");
   scanf("%d",&opc);
 
-  switch
+  switch(opc)
+    {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      default:
+        printf("\n\t Opción no valida");
+    }
+  return 0;
 }
